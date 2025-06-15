be26767d7c0f4faefc60dace100d46e4c277a852
Matara Kemari <drkemari@gmail.com>
2025-06-15 10:50:25 +0300

    arrow

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function expression
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function to square a number
+  const square = (x) => x * x;
+  
+  // Arrow function to add two numbers
+  const add = (a, b) => a + b;
+  
